10898c42a8f1a3eb6c466bcf1f5f9ab63e7da566
j976573957 <j976573957>
2019-03-15 09:54:38 +0800

    创建工程

---
+++ BRCamera/BRCamera/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BRCamera
+//
+//  Created by Mac on 2019/3/15.
+//  Copyright © 2019 Mac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
